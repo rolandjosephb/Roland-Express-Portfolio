694bef7926bc59fb1713bd8943828755bda5cc2b
rolandjosephb <81430200+rolandjosephb@users.noreply.github.com>
2022-09-30 15:15:51 -0400

    last commit

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ router.get('/about', function(req, res, next) {
 
 /* GET Products page. */
 router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products',});
+  res.render('products', { title: 'Products',});
 });
 
 /* GET Services page. */
@@ -32,7 +32,7 @@ router.get('/services', function(req, res, next) {
 
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact',});
+  res.render('contact', { title: 'Contact',});
 
 });
 
